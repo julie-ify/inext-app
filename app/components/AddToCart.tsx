5e01cc1e861c018ab9473bdf3ecc34328414ebf3
Juliana Ifionu <julieifionu@gmail.com>
2024-06-03 23:28:19 -0400

    Install and use daisyUI to add theme and style button

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 const AddToCart = () => {
 	return (
 		<div>
-			<button onClick={() => console.log('hello')}>Add To Cart</button>
+			<button className='btn btn-primary' onClick={() => console.log('hello')}>Add To Cart</button>
 		</div>
 	);
 };
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -4,10 +4,11 @@ import React from 'react';
 interface User {
 	id: number;
 	name: string;
+	email: string;
 }
 
 const UsersPage = async () => {
-	const API_URL = 'https://jsonplaceholder.typicode.com/users'
+	const API_URL = 'https://jsonplaceholder.typicode.com/users';
 	// can fetch data directly from the server component without side effects
 	const res = await fetch(API_URL, {
 		// render on request, don't cache data
@@ -18,12 +19,24 @@ const UsersPage = async () => {
 	return (
 		<div>
 			<h1>Users</h1>
-			<p>{new Date().toLocaleTimeString()}</p>
-			<ul>
-				{users.map((user) => (
-					<li key={user.id}>{user.name}</li>
-				))}
-			</ul>
+			<div className='overflow-x-auto'>
+				<table className='table'>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Email</th>
+						</tr>
+					</thead>
+					<tbody>
+						{users.map((user) => (
+							<tr key={user.id}>
+								<td>{user.name}</td>
+								<td>{user.email}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 };
