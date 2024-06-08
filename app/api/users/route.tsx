5a2b4f18890bd41b2471f386cb51f76335212a58
Juliana Ifionu <julieifionu@gmail.com>
2024-06-08 17:20:35 -0400

    Create and retrieve users using prisma ORM

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,18 +1,43 @@
 import { NextRequest, NextResponse } from 'next/server';
 import schema from './schema';
+// this is used to import prismaclient from the client.ts from the prisma folder
+import client from '@/prisma/client';
+// Prisma object to retrieve some error handling properties
+import { Prisma } from '@prisma/client';
 
-export function GET(request: NextRequest) {
-	return NextResponse.json([
-		{ id: 1, name: 'Juliana' },
-		{ id: 2, name: 'John' },
-	]);
+export async function GET(request: NextRequest) {
+	const users = await client.user.findMany();
+	return NextResponse.json(users);
 }
 
+// Create a user object
 export async function POST(request: NextRequest) {
-	const body = await request.json();
-	// validate
-	const validation = schema.safeParse(body)
-	if (!validation.success)
-		return NextResponse.json(validation.error.errors[0], { status: 400 });
-	return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+	try {
+		const body = await request.json();
+		// validate using zod
+		const validation = schema.safeParse(body);
+		if (!validation.success)
+			return NextResponse.json(validation.error.errors[0], { status: 400 });
+
+		const user = await client.user.create({
+			data: {
+				name: body.name,
+				email: body.email,
+			},
+		});
+		return NextResponse.json(user, { status: 201 });
+	} catch (error) {
+		// handles exceptions
+		if (error instanceof Prisma.PrismaClientKnownRequestError) {
+			// check the error code to determine the error type
+			// handles unique email constraint errors
+			if (error.code === 'P2002') {
+				return NextResponse.json(
+					{ error: 'Invalid Email/Name' },
+					{ status: 400 }
+				);
+			}
+		}
+		throw error;
+	}
 }
