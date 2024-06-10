e8525198ccb796b98165e29f368a399db3d06354
Juliana Ifionu <julieifionu@gmail.com>
2024-06-10 17:43:45 -0400

    Include sign in link to the nav bar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 
 const NavBar = () => {
 	return (
-		<div className="flex bg-slate-200 p-5">
-			<div className="mr-5">
+		<div className="flex bg-slate-200 p-5 space-x-3">
+			<div>
 				<Link href={'/'}>Navbar</Link>
 			</div>
-			<div>
+			<div> 
 				<Link href={'/admin'}>Admin</Link>
+			</div>
+			<div>
+				{/* the signin is exposed to our next auth on api/auth/signin  */}
+				<Link href={'/api/auth/signin'}>Sign in</Link>
 			</div>
 		</div>
 	);
