2f3e963d9c545560ce162808478d4194ab1d197e
Juliana Ifionu <julieifionu@gmail.com>
2024-06-10 22:02:00 -0400

    Use the session hook to check user sessions

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,19 +1,22 @@
+'use client';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react';
 
+// client component because of the hook (useSession) and
 const NavBar = () => {
+	const { status, data: session } = useSession();
+
 	return (
 		<div className="flex bg-slate-200 p-5 space-x-3">
-			<div>
-				<Link href={'/'}>Navbar</Link>
-			</div>
-			<div> 
-				<Link href={'/admin'}>Admin</Link>
-			</div>
-			<div>
-				{/* the signin is exposed to our next auth on api/auth/signin  */}
+			<Link href={'/'}>Navbar</Link>
+			<Link href={'/admin'}>Admin</Link>
+			{status === 'loading' && <p>Loading...</p>}
+			{status === 'authenticated' && <div>{session.user?.name}</div>}
+			{status === 'unauthenticated' && (
+				// the signin is exposed to our next auth on api/auth/signin
 				<Link href={'/api/auth/signin'}>Sign in</Link>
-			</div>
+			)}
 		</div>
 	);
 };
